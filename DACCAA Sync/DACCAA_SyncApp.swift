bca365d083e4f6357deddbf5c53056267a61b1d8
CptCorporate <danny@franklin.uk>
2023-02-06 20:59:22 +0000

    Initial Commit

---
+++ DACCAA Sync/DACCAA_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DACCAA_SyncApp.swift
+//  DACCAA Sync
+//
+//  Created by Danny Franklin on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DACCAA_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
